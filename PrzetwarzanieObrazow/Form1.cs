627331176f5f72431b1e3ca499a34a33b43becf1
pb-275456 <275456@student.pwr.edu.pl>
2025-04-27 19:19:24 +0200

    Update Form1.cs

--- PrzetwarzanieObrazow/Form1.cs
+++ PrzetwarzanieObrazow/Form1.cs
@@ -37,65 +37,32 @@ namespace PrzetwarzanieObrazow
             if (image == null)
                 return;
 
-            var filterTasks = new List<(Func<Bitmap, Bitmap> filter, PictureBox output)>
+            
+            int filtersCount = 4;
+            Thread[] threads = new Thread[filtersCount];
+
+            Bitmap[] results = new Bitmap[filtersCount];
+
+            threads[0] = new Thread(Filters.Negative);
+            threads[1] = new Thread(Filters.Greyscale);
+            threads[2] = new Thread(Filters.mirror);
+            threads[3] = new Thread(Filters.Threshhold);
+
+            for (int i = 0; i < filtersCount; i++)
             {
-                (Filters.Negative, negative),
-                (Filters.Greyscale, greyscale),
-                (Filters.mirror, mirror),
-                (Filters.Threshhold, threshold)
-            };
-            Bitmap result = new Bitmap(image);
+               lock(image) 
+                   results[i] = new Bitmap(image);
+               threads[i].Start(results[i]);
+            }
 
-            int i = 0;
-
-
-            Parallel.ForEach(filterTasks, pair =>
+            for (int i = 0; i < filtersCount; i++)
             {
-                Bitmap copy;
-                lock (image)
-                {
-                    copy = new Bitmap(image); // Thread-safe clone
-                }
-
-                Bitmap result = pair.filter(copy);
-
-                Invoke(() =>
-                {
-                    pair.output.Image = result;
-                    pair.output.Refresh();
-                });
-
-            });
-
-
-
-
-
-            //int filtersCount = 4;
-            //Thread[] threads = new Thread[filtersCount];
-
-            //Bitmap[] results = new Bitmap[filtersCount];
-
-            //threads[0] = new Thread(Filters.Negative);
-            //threads[1] = new Thread(Filters.Greyscale);
-            //threads[2] = new Thread(Filters.mirror);
-            //threads[3] = new Thread(Filters.Threshhold);
-
-            //for (int i = 0; i < filtersCount; i++)
-            //{
-            //    lock(image) 
-            //        results[i] = new Bitmap(image);
-            //    threads[i].Start(results[i]);
-            //}
-
-            //for (int i = 0; i < filtersCount; i++)
-            //{
-            //    threads[i].Join();
-            //}
-            //negative.Image = results[0];
-            //mirror.Image = results[1];
-            //threshold.Image = results[2];
-            //greyscale.Image = results[3];
+               threads[i].Join();
+            }
+            negative.Image = results[0];
+            mirror.Image = results[1];
+            threshold.Image = results[2];
+            greyscale.Image = results[3];
 
         }
     }
